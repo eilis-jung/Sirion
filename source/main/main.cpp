a54505b321cc0f44a243fe8af5ca3123b2dcbc4f
Eilis Jung <eilis-jung@protonmail.com>
2022-11-04 20:33:05 +0100

    temp

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -1,45 +1,10 @@
 
-#include <GL/glut.h>
 #include "core/core.h"
 #include "main.h"
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 
-#pragma region Callbacks
-//static void MouseCallback(int button, int state, int x, int y) {
-//	pApplication2D->mouseCallback(button, state, x, y);
-//}
-//
-//static void MotionCallback(int x, int y) {
-//	pApplication2D->motionCallback(x, y);
-//}
-//
-//
-//static void KeyboardCallback(unsigned char key, int x, int y) {
-//	pApplication2D->keyboardCallback(key, x, y);
-//}
-//static void SpecialKeyboardCallback(int key, int x, int y) {
-//	pApplication2D->specialKeyboardCallback(key, x, y);
-//}
-//
-//static void SpecialKeyboardUpCallback(int key, int x, int y) {
-//	pApplication2D->specialKeyboardUpCallback(key, x, y);
-//}
-//
-//static void ExitCallback() {
-//	pApplication2D->exitCallback();
-//}
-//
-//static void ReshapeCallback(int width, int height) {
-//	pApplication2D->reshapeCallback(width, height);
-//}
-//
-//static void RenderCallback() {
-//	pApplication2D->draw();
-//	pApplication2D->update();
-//}
-#pragma endregion
 
 namespace Sirion
 {
@@ -115,48 +80,9 @@ namespace Sirion
 } // namespace Sirion
 
 
-static void IdleCallback() {
-	glutPostRedisplay();
-}
-
-void renderScene(void) {
-
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-0.5, -0.5, 0.0);
-	glVertex3f(0.5, 0.0, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glEnd();
-
-	glutSwapBuffers();
-}
 
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-
     Sirion::metaExample();
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
-	glutCreateWindow("Lighthouse3D- GLUT Tutorial");
-
-
-	glutDisplayFunc(renderScene);
-	//glutMouseFunc(MouseCallback);
-	//glutMotionFunc(MotionCallback);
-	//glutKeyboardFunc(KeyboardCallback);
-	//glutSpecialFunc(SpecialKeyboardCallback);
-	//glutSpecialUpFunc(SpecialKeyboardUpCallback);
-	//glutReshapeFunc(ReshapeCallback);
-	//atexit(ExitCallback);
-	//glutDisplayFunc(RenderCallback);
-
-	glutIdleFunc(IdleCallback);
-	glutMainLoop();
-
-
-
 
 	return 0;
 }
---
+++ source/meta_parser/parser/parser_header.h
@@ -0,0 +1,9 @@
+#ifndef __PARSER_HEADER_H__
+#define __PARSER_HEADER_H__
+#include  " C:/Users/appce/source/repos/Sirion/source/main/application/application.h"
+#include  "C:/Users/appce/source/repos/Sirion/source/main/core/core.h"
+#include  "C:/Users/appce/source/repos/Sirion/source/main/main.h"
+#include  "C:/Users/appce/source/repos/Sirion/source/main/serializer/reflection.h"
+#include  "C:/Users/appce/source/repos/Sirion/source/main/serializer/serializer.h
+"
+#endif
