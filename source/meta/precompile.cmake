b14295d39f78f1599f8ef01da04bcdeefb791012
Eilis Jung <eilis-jung@protonmail.com>
2022-11-05 11:04:30 +0100

    rearrange parser

--- source/meta/precompile.cmake
+++ source/meta/precompile.cmake
@@ -15,33 +15,16 @@ elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux" )
 	set(PRECOMPILE_PARSER ${PRECOMPILE_TOOLS_PATH}/SirionParser)
     set(sys_include "/usr/include/c++/9/") 
     #execute_process(COMMAND chmod a+x ${PRECOMPILE_PARSER} WORKING_DIRECTORY ${PRECOMPILE_TOOLS_PATH})
-elseif(CMAKE_HOST_APPLE)
-    find_program(XCRUN_EXECUTABLE xcrun)
-    if(NOT XCRUN_EXECUTABLE)
-      message(FATAL_ERROR "xcrun not found!!!")
-    endif()
-
-    execute_process(
-      COMMAND ${XCRUN_EXECUTABLE} --sdk macosx --show-sdk-platform-path
-      OUTPUT_VARIABLE osx_sdk_platform_path_test
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-
-    set(PRECOMPILE_PRE_EXE)
-	set(PRECOMPILE_PARSER ${PRECOMPILE_TOOLS_PATH}/SirionParser)
-    set(sys_include "${osx_sdk_platform_path_test}/../../Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1") 
 endif()
 
 set (PARSER_INPUT ${CMAKE_BINARY_DIR}/parser_header.h)
 set (TEMPLATE_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/meta_parser)
 ### BUILDING ====================================================================================
 set(PRECOMPILE_TARGET "SirionPrecompile")
-
-# Called first time when building target 
-add_custom_target(${PRECOMPILE_TARGET} ALL
-
 message(${PRECOMPILE_PARSER} "${PRECOMPILE_PARAMS_PATH}"  "${PARSER_INPUT}"  "${SIRION_ROOT_DIR}" "${TEMPLATE_ROOT_DIR}" ${sys_include} "Sirion" 0
 )
+# Called first time when building target 
+add_custom_target(${PRECOMPILE_TARGET} ALL
 # COMMAND # (DEBUG: DON'T USE )
 #     this will make configure_file() is called on each compile
 #   ${CMAKE_COMMAND} -E touch ${PRECOMPILE_PARAM_IN_PATH}a
